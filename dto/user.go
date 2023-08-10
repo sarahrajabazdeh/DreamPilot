2fe5eec8c81cb6181f7971a62243cc69a7d7233e
srajabzadeh <sara.rajabzadeh@sorint.it>
2023-08-10 17:38:00 +0200

    added dto

---
+++ dto/user.go
@@ -0,0 +1,16 @@
+package dto
+
+import "github.com/gofrs/uuid"
+
+type Createuserbody struct {
+	Username string `json:"name" maxLength:"255" validate:"required,max=255" example:"sara"`
+	Password string `json:"surname" maxLength:"255" validate:"required,max=255" example:"RJB"`
+	Email    string `json:"email"`
+}
+
+type UserUpdate struct {
+	ID       uuid.UUID `json:"id" validate:"required"`
+	Username string    `json:"username" validate:"required"`
+	Password string    `json:"password" validate:"required"`
+	Email    string    `json:"email" validate:"required"`
+}
