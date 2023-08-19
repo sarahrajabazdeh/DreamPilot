6d0f0970385f29f7bf7e047353c138b59bf1b37b
srajabzadeh <khode.divergent@gmail.com>
2023-08-19 16:24:58 +0200

    bypass auth middleware for now

--- controller/user.go
+++ controller/user.go
@@ -66,13 +66,13 @@ func NoContentResponse(w http.ResponseWriter) {
 // @Success 200 {array} model.User
 // @Router /api/getallusers [get]
 func (ctrl *HttpController) GetAllUsers(w http.ResponseWriter, r *http.Request) {
-	tokenString := r.Header.Get("Authorization")
+	// tokenString := r.Header.Get("Authorization")
 
-	_, err := ctrl.jwt.Authenticate(tokenString)
-	if err != nil {
-		http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
-		return
-	}
+	// _, err := ctrl.jwt.Authenticate(tokenString)
+	// if err != nil {
+	// 	http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
+	// 	return
+	// }
 
 	users, err := ctrl.DS.GetAllUsers()
 	if err != nil {
